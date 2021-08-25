04e74999338e58a20510db1b80069904d886652c
Ilya Nechaev <dolphins.mind777@gmail.com>
2021-08-26 01:25:07 +0300

    build interface

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Илья Нечаев on 26.08.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+ 
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        
+    }
+    
+}
